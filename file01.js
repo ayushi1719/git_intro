a4178d5088eef4f76472f7643a22afa84708f383
AYUSHI GUPTA <ayushigupta1719@gmail.com>
2019-06-05 15:46:32 +0530

    more git information in file01.js

--- file01.js
+++ file01.js
@@ -1,3 +1,4 @@
 console.log('hey,we're learning git')
 console.log('use "git status" to check status')
 console.log('use "git log" to to see history')
+console.log('use "git commit"to register the chnage')
