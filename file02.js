4bc8bc4a5ebfee31b2e014267aa31505a5f6ed74
AYUSHI GUPTA <ayushigupta1719@gmail.com>
2019-06-05 15:57:38 +0530

    sheldon greeter

--- file02.js
+++ file02.js
@@ -1,5 +1,7 @@
 function greet(name)
 {
-	console.log('hey' +name)
+	console.log('knock knock knock' +name)
+	console.log('knock knock knock' +name)
+	console.log('knock knock knock' +name)
 }
 greet('ayushi')
